5fab74457168ceb93b9a3070039a6f906809b0bc
zhushilai <zsl19900608@163.com>
2021-07-08 10:45:59 +0800

    Optional判断空处理

--- src/main/java/Optional/serviceImpl/OptionalTestNull.java
+++ src/main/java/Optional/serviceImpl/OptionalTestNull.java
@@ -1,12 +1,13 @@
 package Optional.serviceImpl;
 import java.util.Optional;
+import java.util.function.Consumer;
 
 /**
  * 判断空对象
  */
 public class OptionalTestNull {
     public static void main(String[] args) {
-        String userName=null;
+        String userName="jimao";
         /**
          * ofNullable 可以运行传值一个空值对象
          * of 不允许传递一个空值对象
@@ -16,9 +17,12 @@ public class OptionalTestNull {
  //       System.out.println(optional.get());
         boolean present=optional.isPresent();
         /**
-         * false 返回false则表示值为空
-         * true  返回true值不为空
+         * false 返回false则不执行下面的方法
+         * true  返回true值执行下面的方法
          */
-        System.out.println(present);
+        if(present) {
+            System.out.println(optional.get());
+        }
+
     }
 }
